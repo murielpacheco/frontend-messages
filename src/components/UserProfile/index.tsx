1e6ce03f65c935940f5dd8f12a2ad4ed6ed64561
Muriel Damazio <murieldps@gmail.com>
2022-03-14 18:53:57 -0300

    Initial commit. Starting project. UserProfile component

---
+++ src/components/UserProfile/index.tsx
@@ -0,0 +1,18 @@
+import styles from "./styles.module.scss";
+
+
+export function UserProfile() {
+   return (
+      <section className={styles.userContainer}>
+         <div className={styles.userAvatar}></div>
+         <span className={styles.userName}>Muriel Damazio</span>
+         <span className={styles.userLocation}>Florianópolis, Brazil</span>
+
+         <div className={styles.userInfos}>
+            <button className={styles.userInfosItem}>Profile</button>
+            <button className={styles.userInfosItem}>About</button>
+            <button className={styles.userInfosItem}>Socials</button>
+         </div>
+      </section>
+   )
+}
